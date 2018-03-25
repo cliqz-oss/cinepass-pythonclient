68d954d750f7caec1be02acaf6bcd0e7939aea69
bhaveshAn <bhaveshanand7@gmail.com>
2018-03-25 18:46:12 +0530

    Fixes #4 Enable package to install deps automatically

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     author='Konstiantyn Snihyr',
     author_email='konstantin.s@cliqz.com',
@@ -7,12 +10,15 @@ setup(
     version='0.2',
     provides=['cinepass'],
     url='https://github.com/cliqz/cinepass-pythonclient',
+    license='MIT',
     packages=[
         'cinepass',
         'cinepass.client',
         'cinepass.client.v4',
     ],
     zip_safe=False,
+    include_package_data=True,
+    install_requires=requirements,
     platforms='any',
     tests_require=[
         'pytest',
